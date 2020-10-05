d6c7abd144c81d014c987b132d3dc0278131ea51
hashim715 <mharus575@gmail.com>
2020-10-05 05:20:26 -0700

    I have also set the redirection and some messages displayed after registration

--- userauth/views.py
+++ userauth/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from .forms import CreateForm
-# Create your views here.
+from django.shortcuts import redirect
+from django.contrib import messages
+# Create your views here
 def UserRegisterView(request):
 	form = CreateForm()
 	if request.method == 'POST':
 		form = CreateForm(request.POST)
 		if form.is_valid():
 			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created ' + user + '.')
+			return redirect('another')
 	context = {'form':form}
 	return render(request, 'practice.html', context)
-
 def another(request):
 	return render(request, 'hello.html')
 
